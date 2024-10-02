2e402b3677f934a9e54ff5c5a9bbd5db4566a5e7
aayush130405 <aayush130405@gmail.com>
2024-10-03 00:29:55 +0530

    adding js script

---
+++ script.js
@@ -0,0 +1,9 @@
+document.getElementById("joinus-form").addEventListener("submit", function(event) {
+    event.preventDefault();
+    alert("Thank you for joining us! We will contact you soon.");
+});
+
+document.getElementById("signin-form").addEventListener("submit", function(event) {
+    event.preventDefault();
+    alert("You have successfully signed in!");
+});
