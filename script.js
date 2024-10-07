db63c33d6ca4e9d9fad834830cc12756a5fe6057
Yogitakanyal15 <kanyalyogita01@gmail.com>
2024-10-08 00:05:56 +0530

    removed unwanted files n structured css file

--- script.js
+++
@@ -1,9 +0,0 @@
-document.getElementById("joinus-form").addEventListener("submit", function(event) {
-    event.preventDefault();
-    alert("Thank you for joining us! We will contact you soon.");
-});
-
-document.getElementById("signin-form").addEventListener("submit", function(event) {
-    event.preventDefault();
-    alert("You have successfully signed in!");
-});
